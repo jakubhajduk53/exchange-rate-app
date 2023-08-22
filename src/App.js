49677dbe625c1e0b448c69d6b7abfdb5b8c78609
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-22 17:09:31 +0200

    feat: add fetch data function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import axios from "axios";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const SITE_URL = process.env.REACT_APP_SITE_URL;
+
 function App() {
-  return <div className="App">Exchange Rate App</div>;
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const { data, error } = await axios.get(`${SITE_URL + API_KEY}/latest/USD`);
+    if (error) {
+      throw new Error(error.message);
+    }
+    if (data) {
+      console.log(data);
+      console.log(data.conversion_rates);
+      setData(Object.entries(data.conversion_rates));
+    }
+  };
+
+  return (
+    <div>
+      <p className="text-3xl">Exchange Rate App</p>
+      <div>
+        {data
+          ? data.map(([name, value]) => {
+              return (
+                <div key={nanoid()}>
+                  {name} : {value}
+                </div>
+              );
+            })
+          : null}
+      </div>
+      <button
+        onClick={() => {
+          fetchData();
+        }}
+        className="text-2xl border rounded-md p-2 bg-blue-500 text-white"
+      >
+        Get Data
+      </button>
+    </div>
+  );
 }
 
 export default App;
