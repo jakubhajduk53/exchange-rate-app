15bd230aed7e323cf8c5b3b1ddd43e501c330b46
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-25 16:39:55 +0200

    chore: sort classes in Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 function Button({ className, type, onClick, value }) {
   const buttonClasses = classNames(
-    "bg-gray-600 shadow-md rounded-xl p-2 sm:p-5 text-sm sm:text-base ",
+    "p-2 sm:p-5 bg-gray-600 shadow-md rounded-xl text-sm sm:text-base ",
     className
   );
   return (
